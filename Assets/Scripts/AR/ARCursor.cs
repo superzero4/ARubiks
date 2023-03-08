11a4acc314efda50661304aa8bafd5d910cffd32
pleyodevpc <71715558+pleyodevpc@users.noreply.github.com>
2023-03-08 13:19:19 +0100

    First draft of Script to handle taping on objects in scene

---
+++ Assets/Scripts/AR/ARCursor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using Sirenix.OdinInspector;
+public class ARCursor : MonoBehaviour
+{
+    [SerializeField]
+    private ARRaycastManager _raycast;
+    [SerializeField, EnumToggleButtons]
+    private TrackableType _tracked;
+    private void Update()
+    {
+        var hits = new List<ARRaycastHit>();
+        if (Input.touchCount > 0)
+            if (_raycast.Raycast(Camera.main.ViewportToScreenPoint(ViewPortpoint()), hits, TrackableType.Planes))
+                hits.ForEach((hit) => hit.trackable.gameObject.GetComponentInChildren<Renderer>().material.color = Color.red);
+    }
+
+    private static Vector3 ViewPortpoint()
+    {
+        return Input.GetTouch(0).position;
+        return new Vector3(.5f, .5f, 0);
+    }
+}

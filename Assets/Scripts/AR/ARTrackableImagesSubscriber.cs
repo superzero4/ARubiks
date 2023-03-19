7dcf2f311469ad27e680e3e7fe4302e894970b54
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-03-19 15:38:23 +0100

    Update ARTrackableImagesSubscriber.cs

--- Assets/Scripts/AR/ARTrackableImagesSubscriber.cs
+++ Assets/Scripts/AR/ARTrackableImagesSubscriber.cs
@@ -3,13 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using System.Linq;
+using Sirenix.OdinInspector;
 
 public class ARTrackableImagesSubscriber : MonoBehaviour
 {
     [SerializeField]
     private ARTrackedImageManager _tracker;
-    [SerializeField]
+    [SerializeField,InfoBox("Add a PlanesController component inside the prefab hierarchy to recolor prefab based on image tracked index")]
     private GameObject _prefabToSpawnOnTracked;
+    [SerializeField, InfoBox("To iterate and find correct scaling of prefab to spawn, you might want to scale your prefab and/or spawn multiple time the prefab on the tracked image with different scale", InfoMessageType = InfoMessageType.Info)]
+    [InfoBox("Might color your prefab differently based on their size, to differentiate them in game, check corresponding function isnide code", InfoMessageType = InfoMessageType.Info)]
+    [InfoBox("Only one value for default behaviour", InfoMessageType = InfoMessageType.Warning, VisibleIf = "_scales.Length!=1")]
+    private float[] _scales;
     private Dictionary<ARTrackedImage, int> _reindexedList;
     private void OnEnable()
     {
@@ -18,20 +24,40 @@ public class ARTrackableImagesSubscriber : MonoBehaviour
     }
     private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
     {
+        ;
         foreach (var im in args.added)
         {
-            var instantiated = Instantiate(_prefabToSpawnOnTracked, im.transform).transform;
-            instantiated.localScale = .08f * Vector3.one;
-            int customIndex = _reindexedList.Count;
-            _reindexedList.Add(im, customIndex);
-            instantiated.GetChild(instantiated.childCount - 1).GetComponent<Renderer>().material.color = GetColorBasedOnIndex(customIndex);
+            foreach (var scale in _scales)
+            {
+                var instantiated = Instantiate(_prefabToSpawnOnTracked, im.transform).transform;
+                instantiated.localScale = scale * Vector3.one;
+                instantiated.localPosition = Vector3.zero;
+                int customIndex = _reindexedList.Count;
+                _reindexedList.TryAdd(im, customIndex);
+                foreach (var plane in instantiated.GetComponentsInChildren<PlanesController>())
+                {
+                    //You can also use scale for specific coloration basedOnIndex
+                    plane.SetColor(GetColorBasedOnIndex(Index));
+                }
+            }
         }
         LogState(args.added, "Begin tracking : ");
         foreach (var added in args.added) { }
         LogState(args.updated, "Keep tracking : ");
         LogState(args.removed, "Loose/Stop tracking : ");
     }
-
+    private Color GetColorBasedOnIndex(float customIndex)
+    {
+        if (customIndex < 1.25f)
+            return Color.red;
+        else if (customIndex < 1.75f)
+            return Color.green;
+        else if (customIndex < 2.75f)
+            return Color.yellow;
+        else if (customIndex < 3.75f)
+            return Color.blue;
+        return UnityEngine.Random.ColorHSV();
+    }
     private Color GetColorBasedOnIndex(int customIndex)
     {
         switch (customIndex)
@@ -46,7 +72,13 @@ public class ARTrackableImagesSubscriber : MonoBehaviour
     private void LogState(List<ARTrackedImage> list, string prefix = "")
     {
         foreach (var image in list)
-            Debug.Log(prefix + " " + image.referenceImage.name + " : " + image.transform.position + " " + image.trackingState + " " + image.transform.GetChild(0)?.name + " " + _reindexedList[image] + ",," + image.name + ",,," /*+ string.Join('\\', image.GetComponents<Component>())*/);
+        {
+            var child = image.transform.GetChild(0);
+            Debug.Log(prefix + " " + image.trackingState + " : " + image.transform.position + " : " + child.name + " : " + child.GetChild(0).name + "," + child.GetChild(1).name + " " + image.referenceImage.name + " " + image.transform.GetChild(0)?.name + " " + _reindexedList[image] + ",," + image.name + ",,," /*+ string.Join('\\', image.GetComponents<Component>())*/);
+            var planes = GetComponentInChildren<PlanesController>();
+            if (planes != null)
+                Debug.Log(planes.transform.lossyScale);
+        }
     }
 
     // Start is called before the first frame update

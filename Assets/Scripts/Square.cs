41188724fc704bb33a835b8e691879a8f71c023b
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-03-23 22:04:28 +0100

    Started investigating how snapping happens and how we'll fix it
    Probably have only one collider per face, and when it hit's find the clostes/shoot a ray from the center of the piece (being it's (0,0) no matter how pieces are placed and then snap it this way so we move the least possible
    Or use this logic with multiple colliders but we'll need to get back to find the center of the shape and then find another collider to use

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -13,6 +13,7 @@ public class Square : MonoBehaviour
         //Detect a piece
         if (other.attachedRigidbody.TryGetComponent<Piece>(out Piece piece) && piece.GetIsFalling())
         {
+            Debug.Log(gameObject.name + " from " + face.name);
             Debug.LogWarning("To do, ensure that connected cubes also update their corresponding squares");
             piece.SetOnFace(pieceTransform);
             face.UpdateFaceColor(index, other.gameObject.GetComponent<MeshRenderer>().material.color);

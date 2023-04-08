eb25c337a6cb3b543c423a8632442aebacc2348a
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-04-08 13:37:54 +0200

    Fix messed up merging by deleting one duplicate func

--- Assets/Scripts/PieceSpawner.cs
+++ Assets/Scripts/PieceSpawner.cs
@@ -53,16 +53,6 @@ public class PieceSpawner : MonoBehaviour
         //StartCoroutine(ActivateLightBeam(piece));
         return piece;
     }
-    //Spawn the piece and set her color
-    public (Piece parent, SubPiece instantiated) SpawnPiece(SubPiece p, Material mat, Vector3 offset, Transform parent = null)
-    {
-        if (parent == null || !parent.TryGetComponent<Piece>(out Piece piece))
-            piece = parent.gameObject.AddComponent<Piece>();
-        var subPiece = Instantiate(p, transform.position + offset, Quaternion.identity, parent);
-        subPiece.GetComponent<MeshRenderer>().material = mat;
-        subPiece.Parent(piece);
-        return (piece, subPiece);
-    }
 
 
     //Activate light beam effect during the falling of the piece

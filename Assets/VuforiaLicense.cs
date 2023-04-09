91cebe06233ae9bc22de73e48e9d60e2fa0806de
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-04-09 22:01:26 +0200

    Ok vulforia

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWRsd2Fhci8vLy8vQUFBQm1kcXhsRStTVGtDRnU1RExteWk5bWZNaUVVNk45dE9GY0txM0k3OHE0eWNoUm50TWg3YXFmR1F3R2tBNkVCMlg4SjdKUkpJTVZaUXNwT3lhdWZaWlZCVXhSOThCY2JtR0ZLbzZ1dksrcEZHRFl0ajloRmIwU1RlWmlVdTVDUGx4SjVzSHFWbmdKbXpDMmJ0blNGWFNkTDlrT1QrTmJOcjJpYU45WHI4TUFsTnhZNTJ4UkpRQ0gvN1ovTTR4VS81SzM3bUdYd1lkRUdGWURVM08vVXptNWhyWWNpc2pWSHFWai9ta21zcGx6RjBCRDhBZTVOazlRSERzQUNZYVZUWjBJU3VqNE0xR09BRll6cUtmQ1hHQVlzQU1LemtvZ3UvL2lxem44VG1ZTmpXYnd5SkVyZ0N6MlZHUVN4YUhObytVOVE0d0J2RFZqYlRlWHd0SzJNZTdtTmRUNmxPdlY4bHFESDkwa2pBQTRBRUg=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
